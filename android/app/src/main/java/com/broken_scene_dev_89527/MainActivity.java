528a7e1f975280ffa0d161a0594d984dae6c1eed
Crowdbotics <engineering@crowdbotics.com>
2023-06-07 14:13:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/broken_scene_dev_89527/MainActivity.java
@@ -0,0 +1,15 @@
+package com.broken_scene_dev_89527;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "broken_scene_dev_89527";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-scene-dev-89527",
+  url: "https://broken-scene-dev-89527.botics.co",
+  api: "https://broken-scene-dev-89527.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
